1ebde03213e8823bb0cfc90cf832163f33404fd5
Andrei <andreidei0406@yahoo.com>
2021-11-05 14:44:10 +0200

    modified main to verify fucntions and to test filesystem library

--- OD_project/OD_project/Source.cpp
+++ OD_project/OD_project/Source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <filesystem>
+#include <cstdlib>
+#include "user.h";
+namespace fs = std::filesystem;
 int main()
 {
-	
+	std::ifstream g("conturi.txt");
+	/*for (int index = 0; index < 2; index++)
+	{
+		User a;
+		std::cin >> a;
+		g << a;
+	}*/
+	fs::path path_obj(fs::temp_directory_path());
+	fs::create_directory("test_filesystem");
+	fs::permissions("test_filesystem", fs::perms::others_all, fs::perm_options::remove);
+	User a("anda2321", "KKl123");
+	if(verify_if_user_exists(g, a) == 0)
+		delete_account(g, a);
 	return 0;
 }
