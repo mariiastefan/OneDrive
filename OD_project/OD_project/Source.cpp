3c474756ee9ea971d58b8c41e2ce72930501eca3
Andrei <andreidei0406@yahoo.com>
2021-11-05 14:58:11 +0200

    comment

--- OD_project/OD_project/Source.cpp
+++ OD_project/OD_project/Source.cpp
@@ -12,11 +12,12 @@ int main()
 		std::cin >> a;
 		g << a;
 	}*/
-	fs::path path_obj(fs::temp_directory_path());
+	/*fs::path path_obj(fs::temp_directory_path());
 	fs::create_directory("test_filesystem");
-	fs::permissions("test_filesystem", fs::perms::others_all, fs::perm_options::remove);
+	fs::permissions("test_filesystem", fs::perms::others_all, fs::perm_options::remove);*/
 	User a("anda2321", "KKl123");
-	if(verify_if_user_exists(g, a) == 0)
+	if(verify_if_user_exists(g, a)) // nu returneaza bine trebuie modificat
+
 		delete_account(g, a);
 	return 0;
 }
