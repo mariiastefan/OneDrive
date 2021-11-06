6fa70815380c7681270951fcd8406594f8ab841c
mariiastefan <mariiastefan17@gmail.com>
2021-11-06 21:05:33 +0200

    added new item in File Class, renamed variables

--- OD_project/OD_project/File.cpp
+++ OD_project/OD_project/File.cpp
@@ -1,11 +1,11 @@
 #include "file.h"
 
-File::File(const std::string& nume, const std::string& data) :
-	m_nume_fisier{ nume }, m_data_fisier{ data }, m_data_actualizare{ data }
+File::File(const std::string& name, const std::string& date) :
+	m_filename{ name }, m_file_date{ date }, m_updated_date{ date }
 {};
 
-void File::ActualizareData(std::string nume_nou, std::string data)
+void File::ActualizareData(std::string new_name, std::string date)
 {
-	if (m_nume_fisier == nume_nou)
-		m_data_actualizare = data;
+	if (m_filename == new_name)
+		m_updated_date = date;
 }
--- OD_project/OD_project/file.h
+++ OD_project/OD_project/file.h
@@ -4,10 +4,12 @@
 
 class File {
 private:
-	std::string m_nume_fisier;
-	std::string m_data_fisier;
-	std::string m_data_actualizare;
+	std::string m_filename;
+	std::string m_file_date;
+	std::string m_updated_date;
+	int m_file_size;
 public:
 	File(const std::string& nume, const std::string& data);
-	void ActualizareData(std::string nume_nou, std::string data);
+	void ActualizareData(std::string new_name, std::string date);
+	int GetSize();
 };
