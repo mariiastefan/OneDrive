7fec2308605346fd2ac321b63eccfde04947180a
Andrei <andreidei0406@yahoo.com>
2021-11-11 14:28:45 +0200

    added window with background image

--- Interface/Interface/Interface.cpp
+++ Interface/Interface/Interface.cpp
@@ -3,8 +3,15 @@
 Interface::Interface(QWidget *parent)
     : QMainWindow(parent)
 {
+
     ui.setupUi(this);
+    QPixmap bg("background.jpg");
+    bg = bg.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bg);
+    this->setPalette(palette);
 }
+
 
 void Interface::on_closeBtn_clicked()
 {
