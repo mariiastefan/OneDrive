14b858e26ef3b297912693b48965b3730bc0365c
Anda-Gianina Palasan <plasananda@yahoo.com>
2021-11-27 22:41:27 +0200

    Added constructor for App class

--- Interface/Interface/App.cpp
+++ Interface/Interface/App.cpp
@@ -1,11 +1,38 @@
 #include "App.h"
-
-App::App(QWidget *parent)
-	: QWidget(parent)
+#include <qstackedwidget.h>
+#include <QVBoxLayout>
+#include <qcombobox.h>
+#include <QCombobox>
+#include <QWidget>
+#include <QLabel>
+#include <QDebug>
+#include <QMessageBox>
+App::App(QWidget* parent)
+    : QMainWindow(parent)
 {
-	ui.setupUi(this);
+
+    ui.setupUi(this);
+    QPixmap bg("background.jpg");
+    bg = bg.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bg);
+    this->setPalette(palette);
+    this->setWindowIcon(QIcon("logo1.jpg"));
+    this->setWindowTitle("OneDrive Search");
 }
 
-App::~App()
-{
-}
+
+//void App::on_search_clicked()
+//{
+//    on_search = true;
+//    bool flag = true;
+//    QString search = ui.lineEdit->text();
+//    if (flag == false)
+//    {
+//        QMessageBox msgBox;
+//        msgBox.setText("It doesn't exist");
+//        msgBox.setStandardButtons(QMessageBox::Ok);
+//        msgBox.exec();
+//    }
+//
+//}
