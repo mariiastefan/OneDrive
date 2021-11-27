e45f666957b39b4615d18123997a3ae5a1872dda
Anda-Gianina Palasan <plasananda@yahoo.com>
2021-11-27 22:07:52 +0200

    Added App class

--- Interface/Interface/App.h
+++ Interface/Interface/App.h
@@ -1,16 +1,22 @@
 #pragma once
-
-#include <QWidget>
+#include <QtWidgets/QMainWindow>
 #include "ui_App.h"
+#include <QPushButton>
 
-class App : public QWidget
+class App : public QMainWindow
 {
 	Q_OBJECT
 
 public:
-	App(QWidget *parent = Q_NULLPTR);
-	~App();
+	App(QWidget* parent = Q_NULLPTR);
+
+private slots:
+	void on_search_clicked();
 
 private:
 	Ui::App ui;
+	bool on_search = false;
+	QPushButton* newButton;
+
+
 };
