daaaf035fdc67765e64701967e359056b5356723
Anda-Gianina Palasan <plasananda@yahoo.com>
2021-12-03 23:26:35 +0200

    added class FolderUser in interface

---
+++ Interface/Interface/FolderUser.cpp
@@ -0,0 +1,166 @@
+#include "FolderUser.h"
+#include <filesystem>
+#include <cstdio>
+#include<iostream>
+
+
+FolderUser::FolderUser()
+{
+	m_nrItems = 0;
+}
+
+FolderUser::FolderUser(std::string userName)
+{
+	m_FolderName = userName;
+	m_nrItems = 0;
+	namespace fs1 = std::filesystem;
+	std::string path = "../../UserFolder";
+	std::wstring pathOrigin = fs1::current_path();
+	fs1::current_path(path);
+	fs1::create_directory(userName);
+	fs1::current_path(pathOrigin);
+
+}
+
+void FolderUser::DeleteFolder(std::string userName)
+{
+	namespace fs1 = std::filesystem;
+	std::string path = "../../UserFolder";
+	std::wstring pathOrigin = fs1::current_path();
+	fs1::current_path(path);
+	std::uintmax_t n = fs1::remove_all(userName);
+	fs1::current_path(pathOrigin);
+}
+std::string FolderUser::GetFolderName() const
+{
+	return m_FolderName;
+}
+int FolderUser::GetNrItems()const
+{
+	return m_nrItems;
+}
+void FolderUser::SetFolderName(const std::string& foldername2)
+{
+	m_FolderName = foldername2;
+}
+
+
+void FolderUser::SetNrItems()
+{
+	namespace fs1 = std::filesystem;
+	std::string path1 = "../../UserFolder";
+	std::wstring pathOrigin = fs1::current_path();
+	path1 += "/";
+	path1 += m_FolderName;
+	fs1::current_path(path1);
+	for (auto const& dir_entry : std::filesystem::directory_iterator{ fs1::current_path() })
+		m_nrItems++;
+	fs1::current_path(pathOrigin);
+}
+
+void FolderUser::AddFile(std::string userName)
+{
+	std::string old_place;
+	std::cout << "introduceti calea catre fisier: ";
+	std::cin >> old_place;
+
+	std::string ItemName;
+	do {
+		std::cout << "cititi numele fisierului: ";
+		std::cin >> ItemName;
+	} while (verify_existItem(ItemName) == true);
+	old_place += "/";
+	old_place += ItemName;
+
+	namespace fs1 = std::filesystem;
+	std::wstring pathOrigin = fs1::current_path();
+	std::string path = "../../UserFolder";
+	path += "/";
+	path += userName;
+	fs1::current_path(path);
+	path += "/";
+	path += ItemName;
+	fs1::copy((old_place), fs1::current_path());
+	fs1::current_path(pathOrigin);
+	m_nrItems++;
+	File aux(ItemName, "data");
+	m_itemUser.push_back(aux);
+}
+
+bool FolderUser::verify_FolderName(FolderUser folder, std::string name)
+{
+	if (folder.GetFolderName() == name)
+		return true;
+	return false;
+}
+
+bool FolderUser::verify_existItem(std::string filename)
+{
+	for (auto& index : m_itemUser)
+	{
+		if (index.GetFilename() == filename)
+			return true;
+	}
+	return false;
+}
+bool FolderUser::FindInFolder(std::string file)
+{
+	for (auto& fil : m_fileName)
+	{
+		if (fil == file)
+			return true;
+	}
+	return false;
+}
+
+std::string FolderUser::SplitFilename(const std::string str)
+{
+	std::size_t found = str.find_last_of("/\\");
+	return str.substr(found + 1);
+}
+
+void FolderUser::DisplayUserFiles()
+{
+	for (auto index : m_fileName)
+		std::cout << index << std::endl;
+}
+
+void FolderUser::SetItemUser()
+{
+	namespace fs1 = std::filesystem;
+	std::string path1 = "../../UserFolder";
+	std::wstring pathOrigin = fs1::current_path();
+	path1 += "/";
+	path1 += m_FolderName;
+	fs1::current_path(path1);
+
+	for (auto const& dir_entry : fs1::directory_iterator{ fs1::current_path() })
+	{
+		std::string aux = fs1::absolute(dir_entry.path()).string();
+		aux = SplitFilename(aux);
+		m_fileName.push_back(aux);
+	}
+
+	fs1::current_path(pathOrigin);
+
+	DisplayUserFiles();
+
+}
+std::uintmax_t FolderUser::GetSizeOfASpecificFolder()
+{
+	std::string name;
+	std::cout << "Scrieti numele username ului pt care doriti sa aflati dimensiunea fisierului: ";
+	std::cin >> name;
+	namespace fs1 = std::filesystem;
+	std::wstring pathOrigin = fs1::current_path();
+	std::string path = "../../UserFolder";
+	path += "/";
+	path += name;
+	uintmax_t size = 0;
+	for (auto const& dir_entry : fs1::directory_iterator{ path })
+	{
+		size += file_size(dir_entry);
+	}
+	return size;
+
+}
---
+++ Interface/Interface/FolderUser.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include<vector>
+#include <filesystem>
+#include"User.h"
+#include "file.h"
+
+class FolderUser {
+private:
+	std::vector<File>m_itemUser;
+	std::vector<std::string>m_fileName;
+	std::string m_FolderName;
+	int m_nrItems;
+public:
+	FolderUser();
+	FolderUser(std::string userName);
+	void DeleteFolder(std::string userName);
+	std::string GetFolderName()const;
+	int GetNrItems()const;
+	void SetFolderName(const std::string& foldername2);
+	void SetNrItems();
+	void AddFile(std::string userName);
+	bool verify_FolderName(FolderUser folder, std::string name);
+	bool verify_existItem(std::string filename);
+	bool FindInFolder(std::string file);
+	std::string SplitFilename(const std::string str);
+	void DisplayUserFiles();
+	void SetItemUser();
+	uintmax_t GetSizeOfASpecificFolder();
+};
