63914b1064981e6acc44fe7b1d4be0820240eb3c
Andrei <andreidei0406@yahoo.com>
2022-01-05 13:31:45 +0200

    changed client

--- Client/Client/client.cpp
+++ Client/Client/client.cpp
@@ -1,104 +1,34 @@
-#include <WinSock2.h>
-#include <Windows.h>
 #include <iostream>
+#include <array>
 
-#pragma comment(lib, "Ws2_32.lib")
-#define Port 54000
+#include "../Network/TcpSocket.h"
 
-SOCKET Socket, Sub;
-WSADATA Winsock;
-sockaddr_in Addr;
-sockaddr_in IncomingAddress;
-int AddressLen = sizeof(IncomingAddress);
-
-int main()
+int main(int argc, char* argv[])
 {
-	WSAStartup(MAKEWORD(2, 2), &Winsock);    // Start Winsock
-
-	if (LOBYTE(Winsock.wVersion) != 2 || HIBYTE(Winsock.wVersion) != 2)    // Check version
-	{
-		std::cerr << "[ERROR] Bad version !";
-		WSACleanup();
-		return 0;
+	// Validate the parameters
+	if (argc != 2) {
+		std::cerr << "usage: " << argv[0] << " server-name" << std::endl;
+		return 1;
 	}
 
-	Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	std::cout << "Starting client..." << std::endl;
 
-	ZeroMemory(&Addr, sizeof(Addr));
-	Addr.sin_family = AF_INET;
-	Addr.sin_port = htons(Port);
-	bind(Socket, (sockaddr*)&Addr, sizeof(Addr));
+	TcpSocket socket;
+	std::cout << "Connecting to server at: " << argv[1] << std::endl;
+	socket.Connect("192.168.2.99", 54000);
 
-	if (listen(Socket, 1) == SOCKET_ERROR)
+	std::string message = "this is a test :)";
+	std::cout << "Sending message: " << message << std::endl;
+	bool result = socket.Send(message.c_str(), message.size());
+	if (result)
 	{
-		std::cerr<<"[ERROR] Listening error\n";
-	}
-	else
-	{
-		std::cout<<"[INFO] Listening ok\n";
-	}
-
-	if (Sub = accept(Socket, (sockaddr*)&IncomingAddress, &AddressLen))
-	{
-		char* ClientIP = inet_ntoa(IncomingAddress.sin_addr);
-		int ClientPort = ntohs(IncomingAddress.sin_port);
-		std::cout << "[INFO] Client conncted!\n";
-		std::cout<<"IP: "<< ClientIP<< ClientPort<<"\n";
-
-		std::cout<<"[INFO] Sending file ... \n";
-
-		FILE* File;
-		char* Buffer;
-		unsigned long Size;
-
-		File = fopen("C:/Users/andre/Desktop/salut.txt", "rb");
-		if (!File)
-		{
-			std::cerr<<"[ERROR] Error while readaing the file\n";
-			getchar();
-			return 0;
-		}
-
-		fseek(File, 0, SEEK_END);
-		Size = ftell(File);
-		fseek(File, 0, SEEK_SET);
-
-		Buffer = new char[Size];
-
-		fread(Buffer, Size, 1, File);
-		char cSize[MAX_PATH];
-		sprintf(cSize, "%i", Size);
-
-		fclose(File);
-		send(Sub, cSize, MAX_PATH, 0); // File size
-
-		int len = Size;
-		char* data = Buffer;
-
-
-		int Offset = 0;
-		while (Size > Offset)
-		{
-			int Amount = recv(Socket, Buffer + Offset, Size - Offset, 0);
-
-			if (Amount <= 0)
-			{
-				break;
-			}
-			else
-			{
-				Offset += Amount;
-				std::cout<<"2\n";
-			}
-		}
-
-
-		free(Buffer);
-		closesocket(Sub);
-		closesocket(Socket);
-		WSACleanup();
+		std::array<char, 512> receiveBuffer;
+		int revieved;
+		socket.Receive(receiveBuffer.data(), receiveBuffer.size(), revieved);
+		std::cout << "Received: ";
+		std::copy(receiveBuffer.begin(), receiveBuffer.begin() + revieved, std::ostream_iterator<char>(std::cout, ""));
+		std::cout << std::endl;
 	}
 
-	getchar();
 	return 0;
 }
