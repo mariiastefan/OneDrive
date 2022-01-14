8b239d63a4707533da496d48c0379ce4b5654a8d
mariiastefan <mariiastefan17@gmail.com>
2022-01-14 13:14:53 +0200

    Revert "wedfgew fwefasfa"
    This reverts commit f47fcb31b34eff421d5ab133b5adc188a179fe5e.

--- Interface/Interface/main.cpp
+++ Interface/Interface/main.cpp
@@ -1,17 +1,16 @@
 #include "Interface.h"
 #include <fstream>
 #include <QtWidgets/QApplication>
-#include "Server.h"
-#include "Client.h"
 //#include "AppWindow.h"
 
 
 int main(int argc, char *argv[])
 {
-    Server a();
-   /* Client b();*/
-   /* QApplication a(argc, argv);
+    QApplication a(argc, argv);
+    
+    /*AppWindow app;
+    app.show();*/
     Interface interface;
     interface.show();
-    return a.exec();*/
+    return a.exec();
 }
